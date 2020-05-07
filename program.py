792fba3b2a7fe4c8ec23f01297b6dd3b27f5663e
Александра Степанова <guga_san@mail.ru>
2020-05-07 11:28:19 +0300

    Base system function

--- program.py
+++ program.py
@@ -1,6 +1,15 @@
-base = 16
-x = int(input())
+base = int(input('Enter base: '))
+x = int(input('Enter a number: '))
+print(hex(x))
+sis_numbers = []
 while x > 0:
-    diget = x % base
-    print(diget, end='')
+    sis_numbers.append(x % base)
     x //= base
+if base > 10:
+    alph_numbers = {10: "a", 11: "b", 12: "c", 13: "d", 14: "e", 15: "f"}
+    for n in range(len(sis_numbers)):
+        if sis_numbers[n] in alph_numbers:
+            sis_numbers[n] = alph_numbers[sis_numbers[n]]
+sis_numbers.reverse()
+for val in sis_numbers:
+    print(val, end='')
